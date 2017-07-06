9884af9259c8f7484ac89c63f91094c67d327162
Dantuluri Rohitha <dantulurirohitha@gmail.com>
2017-07-06 09:36:23 +0530

    Update doctor.go

--- docfolder/doctor.go
+++ docfolder/doctor.go
@@ -3,21 +3,17 @@ package main
 import (
 "errors"
 "fmt"
-//"strconv"
+"strconv"
 "encoding/json"
-
+"strings"
+"github.com/hyperledger/fabric/core/util"
 "github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 var EVENT_COUNTER = "event_counter"
 type ManageDoctor struct {
 }
-type Doctor struct{             // Attributes of a Patient      
-  DoctorID string `json:"DoctorID"`
-}
-
 var DoctorIndexStr = "_Doctorindex"
-
 type Patient struct{             // Attributes of a Patient      
   PatientID string `json:"PatientID"`
   PatientName string `json:"PatientName"`
@@ -25,16 +21,19 @@ type Patient struct{             // Attributes of a Patient
   Problems string `json:"Problems"`
   Gender string `json:"Gender"`
   PatientMobile string `json:"PatientMobile"`
+  Medications string `json:"Medications"`
   Remarks string `json: "Remarks"`
-}
+  PatientEmail string `json: "PatientEmail"`
+  User string `json: "User"`
+  IStatus string `json: "IStatus"`
+  }
 
-func main() {     
+  func main() {     
   err := shim.Start(new(ManageDoctor))
   if err != nil {
     fmt.Printf("Error starting ManageDoctor chaincode: %s", err)
   }
-}
-
+  }
 func (t *ManageDoctor) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
   var msg string
   var err error
@@ -46,7 +45,7 @@ func (t *ManageDoctor) Init(stub shim.ChaincodeStubInterface, function string, a
   fmt.Println("ManageDoctor chaincode is deployed successfully.");
   
   // Write the state to the ledger
-  err = stub.PutState("abc", []byte(msg))     
+  err = stub.PutState("abc", []byte(msg))       //making a test var "abc", I find it handy to read/write to it right away to test the network
   if err != nil {
     return nil, err
   }
@@ -67,17 +66,20 @@ func (t *ManageDoctor) Init(stub shim.ChaincodeStubInterface, function string, a
 func (t *ManageDoctor) Run(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
     fmt.Println("run is running " + function)
     return t.Invoke(stub, function, args)
-}
+  }
 
-func (t *ManageDoctor) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+  func (t *ManageDoctor) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
     fmt.Println("invoke is running " + function)
 
   // Handle different functions
   if function == "init" {                         //initialize the chaincode state, used as reset
     return t.Init(stub, "init", args)
   } else if function == "dupdate_patient" {
-    return t.dupdate_patient(stub,args)
-  }    
+    return t.dupdate_patient(stub, args)
+  } /* else if function == "update_istatus" {
+    return t.update_istatus(stub, args)
+  }*/
+
    fmt.Println("invoke did not find func: " + function)          //error
   
   return nil, errors.New("Received unknown function invocation")
@@ -87,8 +89,8 @@ func (t *ManageDoctor) Query(stub shim.ChaincodeStubInterface, function string, 
   fmt.Println("query is running " + function)
 
   // Handle different functions
-  if function == "get_byPatientID" {                         //Read a Patient by transId
-    return t.get_byPatientID(stub, args)
+  if function == "getPatient_byID" {                         //Read a Patient by transId
+    return t.getPatient_byID(stub, args)
   } else if function == "get_byDoctorID" {
     return t.get_byDoctorID(stub,args)
   } 
@@ -96,129 +98,62 @@ func (t *ManageDoctor) Query(stub shim.ChaincodeStubInterface, function string, 
   return nil, errors.New("Received unknown function query")
 }
 
+func (t *ManageDoctor) getPatient_byID(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3 args")
+	}
+	PatientChaincode := args[0]
+	PatientID := args[1]
+	f1 := "getPatient_byID"
+	queryArgs1 := util.ToChaincodeArgs(f1, PatientID)
+	patientAsBytes, err := stub.QueryChaincode(PatientChaincode, queryArgs1)
+	if err != nil {
+		errStr := fmt.Sprintf("Failed to query chaincode. Got error: %s", err.Error())
+		fmt.Printf(errStr)
+		return nil, errors.New(errStr)
+	}
+	res := Patient{}
+	json.Unmarshal(patientAsBytes, &res)
+	fmt.Println(res)
+	if res.PatientID == PatientID {
+		fmt.Println("Patient found with PatientID : " + PatientID)
+	} else {
+		return nil, errors.New("PatientID not found")
+	}
+	return nil,nil
+}
+func (t *ManageDoctor) get_byDoctorID(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3 args")
+	}
+	PatientChaincode := args[0]
+	DoctorID := args[1]
+	f1 := "get_byDoctorID"
+	queryArgs1 := util.ToChaincodeArgs(f1, DoctorID)
+	patientAsBytes, err := stub.QueryChaincode(PatientChaincode, queryArgs1)
+	if err != nil {
+		errStr := fmt.Sprintf("Failed to query chaincode. Got error: %s", err.Error())
+		fmt.Printf(errStr)
+		return nil, errors.New(errStr)
+	}
+	return patientAsBytes,nil
+}
 func (t *ManageDoctor) dupdate_patient(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-  var jsonResp string
-  var err error
-  fmt.Println("start dupdate_patient")
-
-  PatientID := args[0]
-  Remarks := args[1]
-
-
-  if len(args) != 2 {
-    return nil, errors.New("Incorrect number of arguments. Expecting 6.")
-  }
-  // set PatientID
-  //PatientID := args[0]
-  PatientAsBytes, err := stub.GetState(PatientID)                 //get the patient for the specified patientID from chaincode state
-  if err != nil {
-    jsonResp = "{\"Error\":\"Failed to get state for " + PatientID + "\"}"
-    return nil, errors.New(jsonResp)
-  }
-  //fmt.Print("vesselAsBytes in update vessel")
-  //fmt.Println(vesselAsBytes);
-   res := Patient{}
-  json.Unmarshal( PatientAsBytes, &res)
-  if res.PatientID == PatientID{
-    fmt.Println("Patient found with PatientID : " + PatientID)
-    //fmt.Println(res);
-    
-    res.Remarks = args[1]
-    }
-	Address := res.Address
-	Problems := res.Problems
-	PatientName := res.PatientName
-	Gender := res.Gender
-	PatientMobile := res.PatientMobile
-  
-  //build the CreatePatient json string manually
-  PatientDetails :=  `{`+
-    `"PatientID": "` + PatientID + `" , `+
-    `"Address": "` + Address + `" , `+
-    `"Problems": "` + Problems + `" , `+
-    `"PatientName": "` + PatientName + `" , `+
-    `"Gender": "` + Gender + `" , `+ 
-    `"PatientMobile": "` + PatientMobile + `" , `+ 
-    `"Remarks": "` + Remarks + `" `+
-    `}`
-  err = stub.PutState(PatientID, []byte(PatientDetails))                  //store patient with id as key
-  if err != nil {
-    return nil, err
-  }
-  return nil, nil
-}
-
-func (t *ManageDoctor) get_byPatientID(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
-	 var PatientID, jsonResp string
-  var err error
-  fmt.Println("start get_byPatientID")
-  if len(args) != 1 {
-    return nil, errors.New("Incorrect number of arguments. Expecting ID of the patient to query")
-  }
-  // set PatientID
-  PatientID = args[0]
-  valAsbytes, err := stub.GetState(PatientID)                  //get the PatientID from chaincode state
-  if err != nil {
-    jsonResp = "{\"Error\":\"Failed to get state for " + PatientID + "\"}"
-    return nil, errors.New(jsonResp)
-  }
-  //fmt.Print("valAsbytes : ")
-  //fmt.Println(valAsbytes)
-  fmt.Println("end get_byPatientID")
-  return valAsbytes, nil  
-}
-
-func (t *ManageDoctor) get_byDoctorID(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
-/*var doctorIndex []string
-var DoctorID, jsonResp, errResp string
-  var err error
-fmt.Println("start get_byDoctorID")
-  if len(args) != 1 {
-    return nil, errors.New("Incorrect number of arguments. Expecting ID of the doctor to query")
-  }
- DoctorID = args[0]
- valAsbytes, err := stub.GetState(DoctorID)
- if err != nil {
-    jsonResp = "{\"Error\":\"Failed to get state for " + DoctorID + "\"}"
-    return nil, errors.New(jsonResp)
-  }
-
- json.Unmarshal(valAsbytes, &doctorIndex)
- jsonResp = "{"
-	for i,val := range doctorIndex{
-		fmt.Println(strconv.Itoa(i) + " - looking at " + val + " for get_byDoctorID")
-		valueAsBytes, err := stub.GetState(val)
-		if err != nil {
-			errResp = "{\"Error\":\"Failed to get state for " + val + "\"}"
-			return nil, errors.New(errResp)
-		}
-
-			jsonResp = jsonResp + "\""+ val + "\":" + string(valueAsBytes[:])
-			//fmt.Println("jsonResp inside if")
-			//fmt.Println(jsonResp)
-			if i < len(doctorIndex)-1 {
-				jsonResp = jsonResp + ","
-			}
-		}
-		
-	jsonResp = jsonResp + "}"
-	fmt.Println("end get_byDoctorID")
-	return []byte(jsonResp), nil*/
-	 var DoctorID, jsonResp string
-  var err error
-  fmt.Println("start get_byDoctorID")
-  if len(args) != 1 {
-    return nil, errors.New("Incorrect number of arguments. Expecting ID of the patient to query")
-  }
-  // set PatientID
-  DoctorID = args[0]
-  valAsbytes, err := stub.GetState(DoctorID)                  //get the PatientID from chaincode state
-  if err != nil {
-    jsonResp = "{\"Error\":\"Failed to get state for " + DoctorID + "\"}"
-    return nil, errors.New(jsonResp)
-  }
-  //fmt.Print("valAsbytes : ")
-  fmt.Println(valAsbytes)
-  fmt.Println("end get_byDoctorID")
-  return valAsbytes, nil       
+	if len(args) != 5 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3 args")
+	}
+	PatientChaincode := args[0]
+    PatientID := args[1]
+    Medications := args[2]
+    Remarks := args[3]
+    User := args[4]
+	f1 := "dupdate_patient"
+	queryArgs1 := util.ToChaincodeArgs(f1, PatientID,Medications,Remarks,User)
+	patientAsBytes, err := stub.InvokeChaincode(PatientChaincode, queryArgs1)
+	if err != nil {
+		errStr := fmt.Sprintf("Failed to query chaincode. Got error: %s", err.Error())
+		fmt.Printf(errStr)
+		return nil, errors.New(errStr)
+	}
+	return nil,nil
 }

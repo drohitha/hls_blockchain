7c205a93adef59c738f08c27c66ef7e88653e45b
Dantuluri Rohitha <dantulurirohitha@gmail.com>
2017-07-06 09:35:12 +0530

    Update insuranceprovider.go

--- ipfolder/insuranceprovider.go
+++ ipfolder/insuranceprovider.go
@@ -5,19 +5,15 @@ import (
 "fmt"
 "strconv"
 "encoding/json"
-
+"strings"
+"github.com/hyperledger/fabric/core/util"
 "github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 var EVENT_COUNTER = "event_counter"
 type ManageInsuranceProvider struct {
 }
-type InsuranceProvider struct{             // Attributes of a Patient      
-  InsuranceProviderID string `json:"InsuranceProviderID"`
-}
-
-var InsuranceProviderIndexStr = "_InsuranceProviderindex"
-
+var InsuranceProviderStr = "_InsuranceProviderindex"
 type Patient struct{             // Attributes of a Patient      
   PatientID string `json:"PatientID"`
   PatientName string `json:"PatientName"`
@@ -25,16 +21,19 @@ type Patient struct{             // Attributes of a Patient
   Problems string `json:"Problems"`
   Gender string `json:"Gender"`
   PatientMobile string `json:"PatientMobile"`
+  Medications string `json:"Medications"`
   Remarks string `json: "Remarks"`
-}
+  PatientEmail string `json: "PatientEmail"`
+  User string `json: "User"`
+  IStatus string `json: "IStatus"`
+  }
 
-func main() {     
+  func main() {     
   err := shim.Start(new(ManageInsuranceProvider))
   if err != nil {
     fmt.Printf("Error starting ManageInsuranceProvider chaincode: %s", err)
   }
-}
-
+  }
 func (t *ManageInsuranceProvider) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
   var msg string
   var err error
@@ -46,14 +45,14 @@ func (t *ManageInsuranceProvider) Init(stub shim.ChaincodeStubInterface, functio
   fmt.Println("ManageInsuranceProvider chaincode is deployed successfully.");
   
   // Write the state to the ledger
-  err = stub.PutState("abc", []byte(msg))     
+  err = stub.PutState("abc", []byte(msg))       //making a test var "abc", I find it handy to read/write to it right away to test the network
   if err != nil {
     return nil, err
   }
   
   var empty []string
   jsonAsBytes, _ := json.Marshal(empty)               //marshal an emtpy array of strings to clear the index
-  err = stub.PutState(InsuranceProviderIndexStr, jsonAsBytes)
+  err = stub.PutState(InsuranceProviderStr, jsonAsBytes)
   if err != nil {
     return nil, err
   }
@@ -67,15 +66,20 @@ func (t *ManageInsuranceProvider) Init(stub shim.ChaincodeStubInterface, functio
 func (t *ManageInsuranceProvider) Run(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
     fmt.Println("run is running " + function)
     return t.Invoke(stub, function, args)
-}
+  }
 
-func (t *ManageInsuranceProvider) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+  func (t *ManageInsuranceProvider) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
     fmt.Println("invoke is running " + function)
 
   // Handle different functions
   if function == "init" {                         //initialize the chaincode state, used as reset
     return t.Init(stub, "init", args)
-  }   
+  } else if function == "update_istatus" {
+    return t.update_istatus(stub, args)
+  }/* else if function == "update_istatus" {
+    return t.update_istatus(stub, args)
+  }*/
+
    fmt.Println("invoke did not find func: " + function)          //error
   
   return nil, errors.New("Received unknown function invocation")
@@ -85,8 +89,8 @@ func (t *ManageInsuranceProvider) Query(stub shim.ChaincodeStubInterface, functi
   fmt.Println("query is running " + function)
 
   // Handle different functions
-  if function == "get_byPatientID" {                         //Read a Patient by transId
-    return t.get_byPatientID(stub, args)
+  if function == "getPatient_byID" {                         //Read a Patient by transId
+    return t.getPatient_byID(stub, args)
   } else if function == "get_byInsuranceProviderID" {
     return t.get_byInsuranceProviderID(stub,args)
   } 
@@ -94,61 +98,61 @@ func (t *ManageInsuranceProvider) Query(stub shim.ChaincodeStubInterface, functi
   return nil, errors.New("Received unknown function query")
 }
 
-
-
-func (t *ManageInsuranceProvider) get_byPatientID(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
-	 var PatientID, jsonResp string
-  var err error
-  fmt.Println("start get_byPatientID")
-  if len(args) != 1 {
-    return nil, errors.New("Incorrect number of arguments. Expecting ID of the patient to query")
-  }
-  // set PatientID
-  PatientID = args[0]
-  valAsbytes, err := stub.GetState(PatientID)                  //get the PatientID from chaincode state
-  if err != nil {
-    jsonResp = "{\"Error\":\"Failed to get state for " + PatientID + "\"}"
-    return nil, errors.New(jsonResp)
-  }
-  //fmt.Print("valAsbytes : ")
-  //fmt.Println(valAsbytes)
-  fmt.Println("end get_byPatientID")
-  return valAsbytes, nil  
+func (t *ManageInsuranceProvider) getPatient_byID(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3 args")
+	}
+	PatientChaincode := args[0]
+	PatientID := args[1]
+	f1 := "getPatient_byID"
+	queryArgs1 := util.ToChaincodeArgs(f1, PatientID)
+	patientAsBytes, err := stub.QueryChaincode(PatientChaincode, queryArgs1)
+	if err != nil {
+		errStr := fmt.Sprintf("Failed to query chaincode. Got error: %s", err.Error())
+		fmt.Printf(errStr)
+		return nil, errors.New(errStr)
+	}
+	res := Patient{}
+	json.Unmarshal(patientAsBytes, &res)
+	fmt.Println(res)
+	if res.PatientID == PatientID {
+		fmt.Println("Patient found with PatientID : " + PatientID)
+	} else {
+		return nil, errors.New("PatientID not found")
+	}
+	return nil,nil
+}
+func (t *ManageInsuranceProvider) get_byInsuranceProviderID(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3 args")
+	}
+	PatientChaincode := args[0]
+	InsuranceProviderID := args[1]
+	f1 := "get_byInsuranceProviderID"
+	queryArgs1 := util.ToChaincodeArgs(f1, InsuranceProviderID)
+	patientAsBytes, err := stub.QueryChaincode(PatientChaincode, queryArgs1)
+	if err != nil {
+		errStr := fmt.Sprintf("Failed to query chaincode. Got error: %s", err.Error())
+		fmt.Printf(errStr)
+		return nil, errors.New(errStr)
+	}
+	return patientAsBytes,nil
+}
+func (t *ManageInsuranceProvider) update_istatus(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3 args")
+	}
+	PatientChaincode := args[0]
+    PatientID := args[1]
+    IStatus := args[2]
+	f1 := "update_istatus"
+	queryArgs1 := util.ToChaincodeArgs(f1, PatientID,IStatus)
+	patientAsBytes, err := stub.InvokeChaincode(PatientChaincode, queryArgs1)
+	if err != nil {
+		errStr := fmt.Sprintf("Failed to query chaincode. Got error: %s", err.Error())
+		fmt.Printf(errStr)
+		return nil, errors.New(errStr)
+	}
+	return nil,nil
 }
 
-func (t *ManageInsuranceProvider) get_byInsuranceProviderID(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
-var InsuranceProviderIndex []string
-var InsuranceProviderID, jsonResp, errResp string
-  var err error
-fmt.Println("start get_byInsuranceProviderID")
-  if len(args) != 1 {
-    return nil, errors.New("Incorrect number of arguments. Expecting ID of the InsuranceProvider to query")
-  }
- InsuranceProviderID = args[0]
- valAsbytes, err := stub.GetState(InsuranceProviderID)
- if err != nil {
-    jsonResp = "{\"Error\":\"Failed to get state for " + InsuranceProviderID + "\"}"
-    return nil, errors.New(jsonResp)
-  }
- json.Unmarshal(valAsbytes, &InsuranceProviderIndex)
- jsonResp = "{"
-	for i,val := range InsuranceProviderIndex{
-		fmt.Println(strconv.Itoa(i) + " - looking at " + val + " for get_byInsuranceProviderID")
-		valueAsBytes, err := stub.GetState(val)
-		if err != nil {
-			errResp = "{\"Error\":\"Failed to get state for " + val + "\"}"
-			return nil, errors.New(errResp)
-		}
-
-			jsonResp = jsonResp + "\""+ val + "\":" + string(valueAsBytes[:])
-			//fmt.Println("jsonResp inside if")
-			//fmt.Println(jsonResp)
-			if i < len(InsuranceProviderIndex)-1 {
-				jsonResp = jsonResp + ","
-			}
-		}
-		
-	jsonResp = jsonResp + "}"
-	fmt.Println("end get_byInsuranceProviderID")
-	return []byte(jsonResp), nil
-}

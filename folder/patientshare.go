f8d462cab7c442ee465abcb24acb7ab53ed1c948
Dantuluri Rohitha <dantulurirohitha@gmail.com>
2017-08-02 18:38:18 +0530

    Update patientshare.go

--- folder/patientshare.go
+++ folder/patientshare.go
@@ -431,7 +431,7 @@ func (t *ManagePatient) update_patient(stub shim.ChaincodeStubInterface, args []
   Medications := args[6]
   Remarks := args[7]
   PatientEmail := args[8]
-  User := args[9]
+ // User := args[9]
 
   if len(args) != 10 {
     return nil, errors.New("Incorrect number of arguments. Expecting 9.")
@@ -457,8 +457,9 @@ func (t *ManagePatient) update_patient(stub shim.ChaincodeStubInterface, args []
   res.PatientMobile = args[5]
   res.Medications = args[6]
   res.Remarks = args[7]
- res.User = args[9]
+   //res.User = args[9]
     }
+	User := res.User
      IStatus := res.IStatus
   
   //build the CreatePatient json string manually
